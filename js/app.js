37701d53d8c54a065d2cd2ab73d786d41a25ce5c
MICHAEL J MELTON <meltonm83@gmail.com>
2016-02-22 20:48:22 -0800

    score issue

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 $(document).ready(function(){
 	console.log("ready");
 
-
-
+	/*question1:
+			<p class="question">
+			1. Which of these things are not commonly thrown on the ice by hockey fans in at least one city with an NHL team?
+			</p>
+			<ul class="answers">
+				<li><input type="checkbox" id="cbox1" value="checkbox1">  A)   Rats</li>
+				<li><input type="checkbox" id="cbox2" value="checkbox2">  B)   Octopi</li>
+				<li><input type="checkbox" id="cbox3" value="checkbox3">  C)   Pucks</li>
+				<li><input type="checkbox" id="cbox4" value="checkbox4">  D)   Hats</li>
+				<li><button class="next" type="button">NEXT ---></button></li>
+			</ul>*/
+	/*goals:
+				<li class="genos"><img class="icon" src="images/lamp.gif" alt="lamp"></li>
+				<li class="genos"><img class="icon" src="images/no-goal.gif" alt="lamp"></li>
+				<li class="genos"><img class="icon" src="images/lamp.gif" alt="lamp"></li>
+				<li class="genos"><img class="icon" src="images/lamp.gif" alt="lamp"></li>
+				<li class="genos"><img class="icon" src="images/lamp.gif" alt="lamp"></li>
+				<li class="genos"><img class="icon" src="images/no-goal.gif" alt="lamp"></li>*/
+	
+/*	function ready2Go() {
+		$('#quiz').html('<p class="question">
+			READY?
+			</p>
+		<button class="next" type="button">BEGIN ---></button>');
+		
+		$('#center').appendChild(div);
+*/
 
 
 });
