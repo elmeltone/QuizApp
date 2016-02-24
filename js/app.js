9850ab41c386fa36ee5f0d24502d7926be44fe31
MICHAEL J MELTON <meltonm83@gmail.com>
2016-02-23 18:55:49 -0800

    q format

--- js/app.js
+++ js/app.js
@@ -22,11 +22,31 @@ $(document).ready(function(){
 	
 	var questions = [
 		{"question":"READY?"},
-		{"question":"1. Which of these things are not commonly thrown on the ice by hockey fans in at least one city with an NHL team?", "choiceA":"  A)   Rats", "choiceB":"  B)   Octopi", "choiceC":"  C)   Pucks", "choiceD":"  D)   Hats"},
-		{"question":"2. As of March 2016, only 3 teams have won the title during the last 5 Stanley Cup Championshps. Which of the following teams is not one of them?", "choiceA":"  A)   Los Angeles Kings", "choiceB":"  B)   Boston Bruins", "choiceC":"  C)   Pittsburgh Penguins", "choiceD":"  D)   Chicago Blackhawks"},
-		{"question":'3. Which hockey player is referred to as "The Great One"?', "choiceA":"  A)   Wayne Gretzky", "choiceB":"  B)   Mark Messier", "choiceC":"  C)   Sidney Crosby", "choiceD":"  D)   Patrick Kane"},
-		{"question":"4. Where is the Hockey Hall of Fame?", "choiceA":"  A)   New York City", "choiceB":"  B)   Toronto", "choiceC":"  C)   Montreal", "choiceD":"  D)   Ottawa"},
-		{"question":"5. What is the penalty for fighting during a game in the NHL?", "choiceA":"  A)   None", "choiceB":"  B)   2 min in the penalty box", "choiceC":"  C)   At least 5 min in the penalty box", "choiceD":"  D)   Thrown out of game"}
+		{"question":"1. Which of these things are not commonly thrown on the ice by hockey fans in at least one city with an NHL team?",
+			"choiceA":"  A)   Rats",
+			"choiceB":"  B)   Octopi",
+			"choiceC":"  C)   Pucks",
+			"choiceD":"  D)   Hats"},
+		{"question":"2. As of March 2016, only 3 teams have won the title during the last 5 Stanley Cup Championshps. Which of the following teams is not one of them?",
+			"choiceA":"  A)   Los Angeles Kings",
+			"choiceB":"  B)   Boston Bruins",
+			"choiceC":"  C)   Pittsburgh Penguins",
+			"choiceD":"  D)   Chicago Blackhawks"},
+		{"question":'3. Which hockey player is referred to as "The Great One"?',
+			"choiceA":"  A)   Wayne Gretzky",
+			"choiceB":"  B)   Mark Messier",
+			"choiceC":"  C)   Sidney Crosby",
+			"choiceD":"  D)   Patrick Kane"},
+		{"question":"4. Where is the Hockey Hall of Fame?",
+			"choiceA":"  A)   New York City",
+			"choiceB":"  B)   Toronto",
+			"choiceC":"  C)   Montreal",
+			"choiceD":"  D)   Ottawa"},
+		{"question":"5. What is the penalty for fighting during a game in the NHL?",
+			"choiceA":"  A)   None",
+			"choiceB":"  B)   2 min in the penalty box",
+			"choiceC":"  C)   At least 5 min in the penalty box",
+			"choiceD":"  D)   Thrown out of game"}
 	];
 
 
